d2d19c74165859a5ad65857eae71a5127e6fa8f2
Blankninja <swbnp7@gmail.com>
2019-05-27 13:58:54 -0500

    added a lot of crap

--- testing.js
+++ testing.js
@@ -6,7 +6,41 @@ var controls;
 var prevTime = performance.now();
 var velocity = new THREE.Vector3();
 
+// forward direction for camera
+var forwardDirection = new THREE.Vector3();
+
+// Does the document have pointer lock for mouse controls?
+var havePointerLock = 'pointerLockElement' in document ||
+    'mozPointerLockElement' in document ||
+    'webkitPointerLockElement' in document;
+
+// Main initialization function
 function init() {
+    // sets the request pointer lock to be able to handle any supported browser
+    document.requestPointerLock = document.requestPointerLock ||
+        document.mozRequestPointerLock ||
+        document.webkitRequestPointerLock;
+
+    // sets the exit pointer lock event to be able to handle any supported browser
+    document.exitPointerLock = document.exitPointerLock ||
+        document.mozExitPointerLock ||
+        document.webkitExitPointerLock;
+
+    //if clicked request pointer lock
+    document.onclick = function(event) {
+        document.body.requestPointerLock();
+    }
+
+    // Hook pointer lock state change events
+    document.addEventListener('pointerlockchange', lockChangeAlert, false);
+    document.addEventListener('mozpointerlockchange', lockChangeAlert, false);
+    document.addEventListener('webkitpointerlockchange', lockChangeAlert, false);
+
+    // Hook pointer lock error events
+    document.addEventListener('pointerlockerror', errorCallback, false);
+    document.addEventListener('mozpointerlockerror', errorCallback, false);
+    document.addEventListener('webkitpointerlockerror', errorCallback, false);
+
     // Create a camera
     // 	Set a Field of View (FOV) of 75 degrees
     // 	Set an Apsect Ratio of the inner width divided by the inner height of the window
@@ -24,6 +58,9 @@ function init() {
 
     // Create First Person Controls
     controls = new THREE.FirstPersonControls( camera );
+    controls.noFly = true;
+    controls.object.position.z = 50;
+    controls.object.position.y = 10;
     scene.add( controls.object );
 
     // Create a PlaneGeometry for the floor
@@ -46,6 +83,7 @@ function init() {
     boxMesh = new THREE.Mesh( boxGeometry, boxMaterial );
     // Add the mesh to the scene
     scene.add( boxMesh );
+    boxMesh.position.y = 5;
 
     // Create a WebGL Renderer
     renderer = new THREE.WebGLRenderer();
@@ -60,7 +98,15 @@ function animate() {
     // kinda like interval but better
     requestAnimationFrame(animate);
 
-    playerControls();
+    //update forward direction to camera direction
+    controls.object.getWorldDirection(forwardDirection);
+    console.log(forwardDirection);
+
+    //rotate the cube
+    boxMesh.rotation.y += 0.01;
+
+    // Process player controls
+    //playerControls();
 
     //render what the camera is seeing
     renderer.render(scene, camera);
@@ -70,11 +116,12 @@ function playerControls () {
 
     // Are the controls enabled? (Does the browser have pointer lock?)
     if ( controls.enabled ) {
-
         // Save the current time
         var time = performance.now();
         // Create a delta value based on current time
         var delta = ( time - prevTime ) / 1000;
+
+        camera.getWorldDirection(forwardDirection);
 
         // Set the velocity.x and velocity.z using the calculated time delta
         velocity.x -= velocity.x * 10.0 * delta;
@@ -114,8 +161,40 @@ function playerControls () {
 
         // Save the time for future delta calculations
         prevTime = time;
-
     }
+}
+
+// is called when pointer is locked and the mouse is moved
+function updateMouse(e) {
+    if(e.movementY < 0) {
+        controls.object.rotation.x += 0.01;
+    }else if(e.movementY > 0) {
+        controls.object.rotation.x -= 0.01;
+    }
+    console.log(e.movementX, e.movementY);
+}
+
+// runs when pointer lock state change is detected
+function lockChangeAlert() {
+    // check to see if the element that is pointer locked is equal to the requested event
+    // cause if they're equal it means it worked
+    if (document.pointerLockElement === document.body ||
+        document.mozPointerLockElement === document.body ||
+        document.webkitPointerLockElement === document.body) {
+        console.log('The pointer lock status is now locked');
+        // Pointer was just locked, enable mousemove listener
+        document.addEventListener("mousemove", updateMouse, false);
+    }else {
+        console.log('The pointer lock status is now unlocked');
+        // Pointer was just unlocked, disable mousemove listener
+        document.removeEventListener("mousemove", updateMouse, false);
+        document.exitPointerLock();
+    }
+}
+
+// runs if when trying to change pointer lock states an error is created
+function errorCallback(e) {
+    console.log("Eh, idk pointer lock broke?");
 }
 
 init();
